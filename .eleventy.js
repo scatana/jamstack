15ab888928cf56b83cee6f940c1fb6125f119e63
Sorin Cătană <scatana@users.noreply.github.com>
2020-08-31 22:39:10 -0400

    Dynamic data

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(config) {
   config.addPassthroughCopy("src/js");
 
   return {
+    pathPrefix: "/jamstack/",
     dir: {
       input: "src",
       output: "dist"
---
+++ src/_data/news.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+require("dotenv").config();
+
+module.exports = async () => {
+  try {
+    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${ process.env.NEWS_API_KEY }&pageSize=5`);
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
